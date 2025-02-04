b475f330b3cf277e72b594d492688dc99f83b4c9
parth-18-bansal <parthbansalcoding@gmail.com>
2025-02-05 00:56:45 +0530

    change the app for checking

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, changed python application deployed on the azure app service!"
+    return "Hello, change python application deployed on the azure app service!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
